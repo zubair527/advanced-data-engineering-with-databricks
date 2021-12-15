d44daa37f77dd97897662ba0d7160e367d91364a
SireInsectus <SireInsectus>
2021-12-15 19:33:42 +0000

    Remvoed old BDC copy

--- Applied-Data-Engineering-Solutions-BDC/Includes/bronze-setup.py
+++
@@ -1,7 +0,0 @@
-# Databricks notebook source
-# MAGIC 
-# MAGIC %run ./reset-&-install-datasets
-
-# COMMAND ----------
-
-# MAGIC %run ./new-data $mode="reset"
--- Applied-Data-Engineering-Solutions-BDC/Version-2.0.0.scala
+++
@@ -1,8 +0,0 @@
-// Databricks notebook source
-// MAGIC 
-// MAGIC %md # Course: ade-mod5
-// MAGIC * Version 2.0.0
-// MAGIC * Built 2021-10-28 20:32:07 UTC
-// MAGIC * Git revision: 4887a911815dda4323729dea49fb17557dd14fd4
-// MAGIC 
-// MAGIC Copyright © 2021 Databricks, Inc.
